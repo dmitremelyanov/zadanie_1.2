2f812eb23f3aeb03d05ff3836098272ddc0163e2
DMITRII <dmitremelyanov@yandex.ru>
2014-07-09 18:45:54 +0600

    zadanie_1.2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            double D;
+            double x1, x2;
+
+            Console.Write("Введите значение коэфициента а:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение коэфициента b:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение коэфициента c:");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            D = Math.Pow(b, 2) - 4 * a * c;
+            if (D >= 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / 2 * a;
+                x2 = (-b - Math.Sqrt(D)) / 2 * a;
+                Console.WriteLine("x1= {0}", x1);
+                Console.WriteLine("x2= {0}", x2);
+
+            }
+            else
+                Console.WriteLine("Действительных корней нет");
+
+            Console.ReadKey();
+
+        }
+    }
+}
